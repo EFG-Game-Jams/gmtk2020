0da57f3cd003acfe456fde568b564ef4f8e3d552
SatriAli <allister.wilson@gmail.com>
2020-07-11 22:28:41 +0200

    added DamageOnContact script for spikes

---
+++ Assets/Scripts/DamageOnContact.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnContact : MonoBehaviour
+{
+	public float damage;
+
+	private void OnCollisionEnter(Collider other)
+	{
+		other.GetComponent<Damageable>()?.TakeDamage(damage);
+	}
+}
